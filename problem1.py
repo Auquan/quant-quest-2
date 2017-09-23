bb138f6bb9e6c19ff1e4bb2324fef997778fa18c
Kanav Arora <kanav@stuph.co>
2017-09-23 23:01:51 +0530

    update to problems

--- problem1.py
+++ problem1.py
@@ -12,7 +12,7 @@ class Problem1Solver():
     '''
 
     def getTrainingDataSet(self):
-        return "sampleData"
+        return "trainingData1"
 
     '''
     Returns the stocks to trade.
@@ -59,9 +59,17 @@ class Problem1Solver():
     '''
 
     def getFeatureConfigDicts(self):
+        ma2Dict = {'featureKey': 'ma_5',
+                   'featureId': 'moving_average',
+                   'params': {'period': 5,
+                              'featureName': 'stockTopAskPrice'}}
         ma1Dict = {'featureKey': 'ma_5',
                    'featureId': 'moving_average',
                    'params': {'period': 5,
+                              'featureName': 'basis'}}
+        expma = {'featureKey': 'exponential_moving_average',
+                 'featureId': 'exponential_moving_average',
+                 'params': {'period': 20,
                               'featureName': 'basis'}}
         sdevDict = {'featureKey': 'sdev_5',
                     'featureId': 'moving_sdev',
@@ -70,32 +78,40 @@ class Problem1Solver():
         customFeatureDict = {'featureKey': 'custom_inst_feature',
                              'featureId': 'my_custom_feature',
                              'params': {'param1': 'value1'}}
-        return [ma1Dict, sdevDict, customFeatureDict]
+        return [ma1Dict, sdevDict, expma]
 
     '''
     Using all the features you have calculated in getFeatureConfigDicts, combine them in a meaningful way
-    to compute the fair value as specified in the question for this instrument
+    to compute the fair value as specified in the question
     Params:
     time: time at which this is being calculated
-    instrument: Instrument for which this is being calculated
     instrumentManager: Holder for all the instruments
+    Returns:
+    A Pandas DataSeries with instrumentIds as the index, and the corresponding data your estimation of the fair value
+    for that stock/instrumentId
     '''
 
-    def getFairValue(self, time, instrument, instrumentManager):
-        # dataframe for historical instrument features. The last row of this data frame
-        # would contain the features which are being calculated in this update cycle or for this time.
-        # The second to last row (if exists) would have the features for the previous
-        # time update. Columns will be featureKeys for different features
-        lookbackInstrumentFeatures = instrument.getDataDf()
-        basisFairValue = lookbackInstrumentFeatures.iloc[-1]['ma_5']
+    def getFairValue(self, updateNum, time, instrumentManager):
+        # holder for all the instrument features
+        lookbackInstrumentFeatures = instrumentManager.getLookbackInstrumentFeatures()
 
-        return basisFairValue
+        # dataframe for a historical instrument feature (ma_5 in this case). The index is the timestamps
+        # atmost upto lookback data points. The columns of this dataframe are the stock symbols/instrumentIds.
+        ma5Data = lookbackInstrumentFeatures.getFeatureDf('ma_5')
+
+        # Returns a series with index as all the instrumentIds. This returns the value of the feature at the last
+        # time update.
+        ma5 = ma5Data.iloc[-1]
+
+        return ma5
 
 
 '''
 We have already provided a bunch of commonly used features. But if you wish to make your own, define your own class like this.
 Write a class that inherits from Feature and implement the one method provided.
 '''
+
+
 class MyCustomFeature(Feature):
     ''''
     Custom Feature to implement for instrument. This function would return the value of the feature you want to implement.
@@ -104,39 +120,48 @@ class MyCustomFeature(Feature):
     2. To finally use it in a meaningful way, specify this feature in getFeatureConfigDicts with appropirate feature params.
     Example for this is provided below.
     Params:
+    updateNum: current iteration of update. For first iteration, it will be 1.
+    time: time in datetime format when this update for feature will be run
     featureParams: A dictionary of parameter and parameter values your features computation might depend on.
                    You define the structure for this. just have to make sure these parameters are provided when
                    you wanted to actually use this feature in getFeatureConfigDicts
     featureKey: Name of the key this will feature will be mapped against.
-    currentFeatures: Dictionary with featurekey: featureValue of the instrument features which have been already calculated in this update
-                     cycle. The features are computed sequentially in order of how they appear in your config.
-    instrument: Instrument for which this feature is being calculated
     instrumentManager: A holder for all the instruments
+    Returns:
+    A Pandas series with stocks/instrumentIds as the index and the corresponding data the value of your custom feature
+    for that stock/instrumentId
     '''
     @classmethod
-    def computeForInstrument(cls, featureParams, featureKey, currentFeatures, instrument, instrumentManager):
-        # Current Book Data (dictionary) for the Instrument. This is the last update to the instrument
-        bookData = instrument.getCurrentBookData()
-
+    def computeForInstrument(cls, updateNum, time, featureParams, featureKey, instrumentManager):
         # Custom parameter which can be used as input to computation of this feature
         param1Value = featureParams['param1']
 
-        # dataframe for historical instrument features. The last row of this data frame
-        # would contain the features which are being calculated in this update cycle or for this time.
-        # The second to last row (if exists) would have the features for the previous
-        # time update. Columns will be featureKeys for different features
-        lookbackInstrumentFeaturesDf = instrument.getDataDf()
+        # A holder for the all the instrument features
+        lookbackInstrumentFeatures = instrumentManager.getLookbackInstrumentFeatures()
 
-        return 0
+        # dataframe for a historical instrument feature (basis in this case). The index is the timestamps
+        # atmost upto lookback data points. The columns of this dataframe are the stocks/instrumentIds.
+        lookbackInstrumentBasis = lookbackInstrumentFeatures.getFeatureDf('basis')
+
+        # The last row of the previous dataframe gives the last calculated value for that feature (basis in this case)
+        # This returns a series with stocks/instrumentIds as the index.
+        currentBasisValue = lookbackInstrumentBasis.iloc[-1]
+
+        if param1Value == 'value1':
+            return currentBasisValue * 0.1
+        else:
+            return currentBasisValue * 0.5
 
 
 if __name__ == "__main__":
     if updateCheck():
-        print 'Your version of the auquan toolbox package is old. Please update by running the following command:'
-        print 'pip install -U auquan_toolbox'
-    problem1Solver = Problem1Solver()
-    tsParams = FairValueTradingParams(problem1Solver)
-    tradingSystem = TradingSystem(tsParams)
-    # Set onlyAnalyze to True to quickly generate csv files with all the features
-    # Set onlyAnalyze to False to run a full backtest
-    tradingSystem.startTrading(onlyAnalyze=False, shouldPlot=False)
+        print('Your version of the auquan toolbox package is old. Please update by running the following command:')
+        print('pip install -U auquan_toolbox')
+    else:
+        problem1Solver = Problem1Solver()
+        tsParams = FairValueTradingParams(problem1Solver)
+        tradingSystem = TradingSystem(tsParams)
+        # Set onlyAnalyze to True to quickly generate csv files with all the features
+        # Set onlyAnalyze to False to run a full backtest
+        # Set makeInstrumentCsvs to False to not make instrument specific csvs in runLogs. This improves the performance BY A LOT
+        tradingSystem.startTrading(onlyAnalyze=False, shouldPlot=False, makeInstrumentCsvs=True)
