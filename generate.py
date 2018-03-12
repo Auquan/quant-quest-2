b47af131c6bc55d5d893563ae1a0875d430af93f
Team Auquan <32048185+TeamAuquan@users.noreply.github.com>
2018-03-12 21:36:48 +0530

    Add files via upload

--- generate.py
+++ generate.py
@@ -7,6 +7,19 @@ try:
 except ImportError:
     from urllib.request import urlopen
 
+from pandas.tseries.frequencies import to_offset
+from functools import partial
+
+
+def resampleData(series, period):
+    return series.groupby(partial(round, freq=period))
+
+def round(t, freq):
+    freq = to_offset(freq)
+    return pd.Timestamp((t.value // freq.delta.value) * freq.delta.value)
+
+print('MAKE SURE you have specified the correct dataset and logFolder')
+print('')
 if len(sys.argv)==1:
 	datasetId = 'trainingW5_trainingData'
 	folders = next(os.walk('runLogs/'))[1]
@@ -18,7 +31,7 @@ elif len(sys.argv)==2:
 else:
 	datasetId = sys.argv[1]
 	folders = sys.argv[2:]
-	print('Calculating for dataset %s and runLogs '%datasetId).join([str(x) for x in folders])
+	print(('Calculating for dataset %s and runLogs '%datasetId).join([str(x) for x in folders]))
 	
 try:
 	mktReturnsFileName = 'historicalData/%s/market_returns.csv'%datasetId
@@ -38,17 +51,25 @@ try:
 	data = None
 	for logFolder in folders:
 		print('Calculating Metrics for %s'%logFolder)
+		temp = pd.read_csv('runLogs/'+logFolder+'/marketFeatures.csv',index_col=0, parse_dates=True)
 		try:
 			temp = pd.read_csv('runLogs/'+logFolder+'/marketFeatures.csv',index_col=0, parse_dates=True)
 		except IOError:
 			continue
 		returns = temp['capital'].diff()/temp['capitalUsage'].max()
 		returns.fillna(0, inplace=True)
-		if returns.index.equals(mkt.index) or returns.index.isin(mkt.index).all():
-			metrics = np.polyfit(mkt['returns'][returns.index].values, returns.values, 1)
-			print('Alpha:%.3f%% Beta: %.3f'%(metrics[1]*100, metrics[0]) )
-
-		else:
+		sampledreturns = resampleData(returns, '1D').last()
+        sampledreturns[1:] = np.diff(sampledreturns)
+        sampledreturns = sampledreturns/temp['capitalUsage'].max()
+        
+        cumret = (mkt['returns'] + 1).cumprod()
+        sampledDf = resampleData(cumret, '1D').last()
+        if sampledreturns.index.equals(sampledDf.index) or sampledreturns.index.isin(sampledDf.index).all():
+            rets = (sampledDf/sampledDf.shift(1))-1
+            rets[0] = mkt.iloc[0]
+            metrics = np.polyfit(rets[sampledreturns.index].values, sampledreturns.values, 1)
+            print('Alpha:%.3f%% Beta: %.3f'%(metrics[1]*100, metrics[0]) )
+        else:
 			print('Dates do not match. Not the same backtest')
 except:
 	print('Market Returns file not found')
